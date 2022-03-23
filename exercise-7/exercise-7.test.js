aab020dada0f6dc7f3d00d63d051fd8398ff0082
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-23 14:29:02 -0700

    Query written and test passing for exercise-7.

--- exercise-7/exercise-7.test.js
+++ exercise-7/exercise-7.test.js
@@ -4,8 +4,10 @@ const pool = require('../lib/utils/pool');
 describe('exercise 7', () => {
   afterAll(() => pool.end());
 
-  it('find customer ranking by total film time watched', async() => {
-    const { rows } = await pool.query(fs.readFileSync(`${__dirname}/exercise-7.sql`, 'utf-8'));
+  it('find customer ranking by total film time watched', async () => {
+    const { rows } = await pool.query(
+      fs.readFileSync(`${__dirname}/exercise-7.sql`, 'utf-8')
+    );
     expect(rows).toEqual(require('./result.json'));
   });
 });
