ae70add9a13477f87e92096b69ff357ba58f219b
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-23 15:03:50 -0700

    Query written and test passing for exercise-9.

--- exercise-9/exercise-9.test.js
+++ exercise-9/exercise-9.test.js
@@ -4,8 +4,10 @@ const pool = require('../lib/utils/pool');
 describe('exercise 9', () => {
   afterAll(() => pool.end());
 
-  it('find ROLLUP of amount made per year, month, day', async() => {
-    const { rows } = await pool.query(fs.readFileSync(`${__dirname}/exercise-9.sql`, 'utf-8'));
+  it('find ROLLUP of amount made per year, month, day', async () => {
+    const { rows } = await pool.query(
+      fs.readFileSync(`${__dirname}/exercise-9.sql`, 'utf-8')
+    );
     expect(rows).toEqual(require('./result.json'));
   });
 });
