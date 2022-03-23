33779672ded1508aa4d292fc13bd7f29e5b1beba
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-23 12:23:34 -0700

    Query written and test passing for exercise-2.

--- exercise-2/exercise-2.test.js
+++ exercise-2/exercise-2.test.js
@@ -4,8 +4,10 @@ const pool = require('../lib/utils/pool');
 describe('exercise 2', () => {
   afterAll(() => pool.end());
 
-  it('find the top 10 cities with the most total rental payment amount', async() => {
-    const { rows } = await pool.query(fs.readFileSync(`${__dirname}/exercise-2.sql`, 'utf-8'));
+  it('find the top 10 cities with the most total rental payment amount', async () => {
+    const { rows } = await pool.query(
+      fs.readFileSync(`${__dirname}/exercise-2.sql`, 'utf-8')
+    );
     expect(rows).toEqual([
       { city: 'Saint-Denis', sum: '211.55' },
       { city: 'Cape Coral', sum: '208.58' },
